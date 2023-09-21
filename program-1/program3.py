6a06043362fd5ca2e90fdf7136c233321515f8c6
Aswathi-Gopi <139220649+Aswathi-Gopi@users.noreply.github.com>
2023-09-21 15:14:42 +0530

    Update program3.py
    added try catch block

--- program-1/program3.py
+++ program-1/program3.py
@@ -1,18 +1,18 @@
 def is_valid_ip_address(ip_address):
-    # Split the IP address into its components
-    components = ip_address.split('.')
+    try:
+        # Split the IP address into its components
+        components = ip_address.split('.')
 
-    # Check that there are exactly 4 components
-    if len(components) != 4:
-        return False
+        # Check that there are exactly 4 components
+        if len(components) != 4:
+            return False
 
-    # Check that each component is an integer between 0 and 255
-    for component in components:
-        try:
+        # Check that each component is an integer between 0 and 255
+        for component in components:
             value = int(component)
             if value < 0 or value > 255:
                 return False
-        except ValueError:
-            return False
 
-    return True
+        return True
+    except (ValueError, AttributeError):
+        return False
